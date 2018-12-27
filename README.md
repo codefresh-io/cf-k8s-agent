ff13d482f8473e3d41bcc8f5c398e6640f6d5fb9
andrii.shaforostov <andrii@codefresh.io>
2018-12-27 12:35:19 +0200

    watching deployments, sending changes to webhook tester

---
+++ README.md
@@ -0,0 +1,14 @@
+docker build -t andriicodefresh/cf-k8s-agent .
+docker push andriicodefresh/cf-k8s-agent
+docker run -d andriicodefresh/cf-k8s-agent --port 9020:9020
+docker start cf-k8s-agent
+
+kubectl apply -f agent-deployment.yaml
+kubectl apply -f agent-service.yaml
+
+kubectl delete -f agent-deployment.yaml
+
+minikube start --kubernetes-version=v1.7.0 --extra-config=apiserver.Authorization.Mode=RBAC
+#kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
+kubectl create clusterrolebinding binding1 --clusterrole=cluster-admin --user=system:serviceaccount:default:default
+minikube dashboard
--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
 
-var indexRouter = require('./api/index');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const { Client } = require('kubernetes-client');
+const { config } = require('kubernetes-client');
+const JSONStream = require('json-stream');
+const rp = require('request-promise');
+
+let counter = 0;
+
+async function getClient() {
+    const client = new Client({ config: config.getInCluster() });
+    await client.loadSpec();
+    return client;
+}
+
+async function watch(client) {
+    const stream = client.apis.apps.v1.watch.namespaces('default').deployments.getStream();
+    const jsonStream = new JSONStream();
+    stream.pipe(jsonStream);
+    jsonStream.on('data', async (obj) => {
+        console.log('Event: ', JSON.stringify(obj, null, 2));
+        rp({
+            method: 'POST',
+            uri: `https://webhook.site/4717800e-cc87-4da0-a44b-585ef63e2531`,
+            body: obj,
+            json: true,
+        }).then(console.log, console.error);
+        counter++;
+        if (counter === 4) stream.abort();
+    });
+}
+
+const indexRouter = require('./api/index');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,5 +46,20 @@ app.use(cookieParser());
 app.use(bodyParser());
 
 app.use('/', indexRouter);
+
+getClient()
+    .then(async (client) => {
+        const namespaces = await client.api.v1.namespaces.get();
+        console.log('namespaces:', JSON.stringify(namespaces));
+        rp({
+            method: 'POST',
+            uri: `https://webhook.site/4717800e-cc87-4da0-a44b-585ef63e2531`,
+            body: namespaces,
+            json: true,
+        }).then(console.log, console.error);
+        return watch(client);
+    })
+    .catch(console.error);
+
 
 module.exports = app;
