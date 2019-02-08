a480bf3a3f129597dfbaf8d8e2d71d8e7a22cce8
andrii.shaforostov <andrii@codefresh.io>
2019-02-08 17:35:08 +0200

    sending updates in package (first stage)

--- src/api/codefresh.api.js
+++ src/api/codefresh.api.js
@@ -8,12 +8,39 @@ const MetadataFilter = require('../filters/MetadataFilter');
 let metadataFilter;
 let counter;
 
+let eventsPackage = [];
+
+const sendPackage = (events) => {
+    const { length } = events;
+    if (!length) return;
+
+    const uri = config.apiUrl.replace('{path}', '/events');
+    const options = {
+        method: 'POST',
+        uri,
+        body: events,
+        headers: {
+            'authorization': config.token,
+            'x-cluster-id': config.clusterId,
+        },
+        json: true,
+    };
+
+    global.logger.debug(`Sending package with ${length} element(s).`);
+    rp(options)
+        .then(() => {
+            // events.data = _.drop(events.data, length);
+            events.splice(0, length);
+        })
+        .catch(global.logger.error);
+};
+
 /**
  * Send cluster event to monitor
  * @param obj - data for sending
  * @returns {Promise<void>}
  */
-const sendEvents = async (obj) => {
+const sendEvents = (obj) => {
     let data = _.cloneDeep(obj);
 
     if (data.kind === 'Status') {
@@ -30,21 +57,12 @@ const sendEvents = async (obj) => {
 
     data.counter = counter++;
 
-    const uri = config.apiUrl.replace('{path}', '/events');
-    const options = {
-        method: 'POST',
-        uri,
-        body: data,
-        headers: {
-            'authorization': config.token,
-            'x-cluster-id': config.clusterId,
-        },
-        json: true,
-    };
-
-    global.logger.debug(`Sending event. Cluster: ${config.clusterId}. ${data.object.kind}. ${obj.object.metadata.name}. ${data.type}`);
+    global.logger.debug(`ADD event to package. Cluster: ${config.clusterId}. ${data.object.kind}. ${obj.object.metadata.name}. ${data.type}`);
     global.logger.debug(`-------------------->: ${JSON.stringify(data.object)} :<-------------------`);
-    rp(options).catch(global.logger.error);
+    eventsPackage.push(data);
+    if (eventsPackage.length === 10) {
+        sendPackage(eventsPackage);
+    }
 };
 
 /**
@@ -93,6 +111,8 @@ async function getMetadata() {
     global.logger.debug(`Get metadata from ${uri}.`);
     return rp(options);
 }
+
+setInterval(sendPackage.bind(null, eventsPackage), 2000);
 
 module.exports = {
     sendEvents,
