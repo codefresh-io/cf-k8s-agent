b09c8235b79b71447f9d4aaec7e4e78b3d2755fb
pashavictorovich <pavel@codefresh.io>
2019-11-20 16:48:51 +0200

    process release one by one

--- src/kubernetes/listener/pull/handler/release.js
+++ src/kubernetes/listener/pull/handler/release.js
@@ -11,28 +11,27 @@ class ReleaseHandler {
     async handle(kind, items) {
         const codefreshApi = require('../../../../api/codefresh.api');
 
-        const extendedItems = await Promise.all(items.map((item) => {
-            item.kind = 'Release';
-            return releaseMetadataFactory.create(item, codefreshApi.getMetadataFilter()).catch(e => logger.error(e));
-        }));
-
-        const filtered = extendedItems.filter(item => item && item.object && item.object.release);
-
-        logger.info(`Prepare to send ${filtered.length} ${kind}s`);
+        logger.info(`Prepare to send ${items.length} ${kind}s`);
 
         await codefreshApi.clearInfo({
             kind: 'Release'
         });
-        const chunks = _.chunk(filtered, 1);
-        return Promise.map(chunks.map((chunk) => {
-            return codefreshApi.sendPackageWithoutLock([{
-                object: _.head(chunk).object,
-                type: 'ADDED',
-                counter: 1,
-                kind: 'Release'
-            }]);
-        }), (job) => { return job; }, { concurrency: 5 });
 
+        for (const item of items) {
+            item.kind = 'Release';
+            const release = await releaseMetadataFactory.create(item, codefreshApi.getMetadataFilter())
+                .catch(e => logger.error(e));
+            if (_.get(release, 'object.release')) {
+                return codefreshApi.sendPackageWithoutLock([{
+                    object: release.object,
+                    type: 'ADDED',
+                    counter: 1,
+                    kind: 'Release'
+                }]);
+            }
+        }
+
+        return Promise.resolve();
     }
 
 }
