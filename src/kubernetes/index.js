48ea98f569494849fcf2f4bbf05956253e0fdf8b
pashavictorovich <pavel@codefresh.io>
2020-02-13 16:20:34 +0200

    Enrich service retrieval with new logs

--- src/kubernetes/index.js
+++ src/kubernetes/index.js
@@ -1,14 +1,25 @@
 const _ = require('lodash');
+const Promise = require('bluebird');
+
 const KubeManager = require('@codefresh-io/kube-integration/lib/kube.manager');
 const { clientFactory, resolveConfig } = require('./client');
+const logger = require('../logger');
 
 const kubeManager = new KubeManager(resolveConfig());
 
-function prepareService(service) {
+async function prepareService(service) {
     const namespace = _.get(service, 'metadata.namespace');
     const name = _.get(service, 'metadata.name');
+    logger.info(`Process service ${name} and get detailed info in ${namespace}`);
     const serviceController = kubeManager.getServiceController(namespace);
-    return serviceController.describeFull(name, namespace);
+    try {
+        return await serviceController.describeFull(name, namespace);
+    } catch (error) {
+        logger.info(`Process service ${name} in ${namespace} was failed`);
+        logger.info(error.stack);
+    }
+    return Promise.resolve();
+
 }
 
 module.exports = {
