4027e77ba5f794149f3417e825f33dd301bcab3b
pashavictorovich <pavel@codefresh.io>
2020-02-15 15:40:03 +0200

    move handle outside

--- src/actions/task.listener.js
+++ src/actions/task.listener.js
@@ -7,25 +7,26 @@ const config = require('../config');
 
 class TaskListener {
 
+    async _handle() {
+        const tasks = await codefreshAPI.getPendingTasks();
+
+        await Promise.all(tasks.map((task) => {
+            const { namespace, release, revision } = task.context;
+            return kubernetes.createPod(config.namespace, release, revision)
+                .catch((error) => {
+                    logger.error(`Cant create pod ${namespace}, ${release}, ${revision}, ${error.stack}`);
+                });
+        }));
+
+    }
+
     listen() {
 
+        const that = this;
         logger.info(`Start listen and process events for rollback in namespace ${config.namespace}`);
 
-        async function handle() {
-            const tasks = await codefreshAPI.getPendingTasks();
-
-            await Promise.all(tasks.map((task) => {
-                const { namespace, release, revision } = task.context;
-                return kubernetes.createPod(config.namespace, release, revision)
-                    .catch((error) => {
-                        logger.error(`Cant create pod ${namespace}, ${release}, ${revision}, ${error.stack}`);
-                    });
-            }));
-
-        }
-
         setInterval(() => {
-            handle();
+            that._handle();
             kubernetes.clearCompletedPods(config.namespace);
         }, 10000);
     }
