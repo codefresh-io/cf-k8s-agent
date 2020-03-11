112a10b1aa9e4f9e51954fec66e0d2877a90365a
alex-codefresh <a.cheshko@codefresh.io>
2020-03-11 10:14:37 +0300

    Add changes related to  SAAS-6206

---
+++ src/k8s-resources/NamespaceStub.js
@@ -0,0 +1,38 @@
+
+// using in case if we not need retrieve all namespaces ( in case not cluster role )
+class NamespaceStub {
+
+    constructor(namespace) {
+        this.namespace = namespace;
+    }
+
+    get() {
+        return {
+            statusCode: 200,
+            body: {
+                kind: 'NamespaceList',
+                apiVersion: 'v1',
+                metadata: {
+                    resourceVersion: '6458782'
+                },
+                items: [
+                    {
+                        metadata: {
+                            name: this.namespace,
+                            uid: '8539f722-6200-11ea-b4c3-42010a9600aa',
+                            resourceVersion: '5888557'
+                        },
+                        spec: {
+                        },
+                        status: {
+                            phase: 'Active'
+                        }
+                    }
+                ]
+            }
+        };
+    }
+
+
+}
+module.exports = NamespaceStub;
